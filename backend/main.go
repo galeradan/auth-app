48e5a7acecac74ddb8b26e1cbe1d50e4ca48830f
Dan Galera <danerick.galera@gmail.com>
2021-08-20 15:56:52 +0800

    Adds comments

--- backend/main.go
+++ backend/main.go
@@ -7,12 +7,15 @@ import (
 )
 
 func main() {
+	// Initialize connection to database
 	_, err := gorm.Open(mysql.Open("root:root@/auth_app_db"), &gorm.Config{})
 
+	// Report database connection error
 	if err != nil {
 		panic("Could not connect to database")
 	}
 
+	// Instantiate new fiber and setup index route
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
