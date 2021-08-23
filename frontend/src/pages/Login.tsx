09b9b5051a293c84b6e384e6a8f712c5a7948a32
Dan Galera <danerick.galera@gmail.com>
2021-08-23 17:05:29 +0800

    Removes label, Includes register form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,10 +2,10 @@ const Login = () => {
   return (
     <form className="form-signin">
       <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label htmlFor="inputEmail" className="sr-only">Email address</label>
-      <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required/>
-      <label htmlFor="inputPassword" className="sr-only">Password</label>
-      <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+      
+      <input type="email" id="email" className="form-control" placeholder="Email address" required/>
+      <input type="password" id="password" className="form-control" placeholder="Password" required/>
+
       <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     </form>
   );
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Register = () => {
   return (
-      <>
-        <h1>Register Page</h1>
-      </>
-       );
+    <form className="form-signin">
+      <h1 className="h3 mb-3 font-weight-normal">Please Register</h1>
+
+      <input type="text" id="name" className="form-control" placeholder="Name" required/>
+      <input type="email" id="email" className="form-control" placeholder="Email address" required/>
+      <input type="password" id="password" className="form-control" placeholder="Password" required/>
+
+      <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+  );
 }
 export default Register
