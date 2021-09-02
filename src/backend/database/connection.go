b8c5e67cfcb84ca7c81e0a1ccf488a4209ff07a7
galeradan <danerick.galera@gmail.com>
2021-09-02 22:10:10 +0800

    Remove db env

--- src/backend/database/connection.go
+++ src/backend/database/connection.go
@@ -19,15 +19,9 @@ func Connect() {
 	dbName := utils.GetEnv("DB_NAME")
 	dbHost := utils.GetEnv("DB_HOST")
 	dbPORT := utils.GetEnv("DB_PORT")
-	dbEnv := utils.GetEnv("DB_ENV")
-	dsn := ""
 
 	// builds the data source name
-	if dbEnv == "production" {
-		dsn = fmt.Sprintf("%s:%s@%s/%s", username, password, dbHost, dbName)
-	} else {
-		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, dbHost, dbPORT, dbName)
-	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, dbHost, dbPORT, dbName)
 
 	// Initialize connection to database
 	sqlDB, err := sql.Open("mysql", dsn)
